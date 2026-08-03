a784cf7114f454238fe0e18ad8b18f82757bfd96
alexleonaguila <146151423+alexleonaguila@users.noreply.github.com>
2026-08-03 16:14:55 -0500

    Remove scroll indicator from Hero component
    Removed the scroll indicator from the Hero component.

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -98,11 +98,7 @@ export function Hero() {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 opacity-40">
-        <span className="text-white text-xs tracking-[0.2em] uppercase">Scroll</span>
-        <ArrowDown size={16} className="text-[#c9a227] animate-bounce" />
-      </div>
+  
 
       {/* Bottom gold line */}
       <div className="absolute bottom-0 left-0 right-0 h-px bg-gradient-to-r from-transparent via-[#c9a227]/40 to-transparent" />
